ba4e1a06c4dd4927d3508fcfb15249a21cbeec72
Pablotechedu <167021133+Pablotechedu@users.noreply.github.com>
2025-02-04 18:43:37 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeToHelloDom() {
+  document.getElementById("header").textContent = "Hello Dom!";
+}
+
+function changeToHelloWorld() {
+  document.getElementById("header").textContent = "Hello Worl!";
+}
+
+function addHelloDom() {
+  document.getElementById("header").textContent += " Hello DOM!";
+}
